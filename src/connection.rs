774b9878631de6c4a34a564b722f694296b8a190
darcy <acednes@gmail.com>
2024-10-15 11:54:30 +1100

    doc(comments): `connection`

--- src/connection.rs
+++ src/connection.rs
@@ -9,28 +9,34 @@ use crate::{
 
 type Result<T> = io::Result<T>;
 
+/// Connection for Minecraft server
 #[derive(Debug)]
 pub struct Connection {
     stream: TcpStream,
 }
 
 impl Connection {
-    const DEFAULT_ADDRESS: &'static str = "127.0.0.1:4711";
+    /// Default server address and port for ELCI
+    pub const DEFAULT_ADDRESS: &'static str = "127.0.0.1:4711";
 
+    /// Create a new connection with the default server address
     pub fn new() -> Result<Self> {
         Self::with_address::<&str>(Self::DEFAULT_ADDRESS)
     }
 
+    /// Create a new connection with a specified server address
     pub fn with_address<A>(addr: impl ToSocketAddrs) -> Result<Self> {
         let stream = TcpStream::connect(addr)?;
         Ok(Self { stream })
     }
 
+    /// Serialize and send a command to the server
     fn send(&mut self, command: Command) -> Result<()> {
         self.stream.write_all(command.build().as_bytes())?;
         Ok(())
     }
 
+    /// Receive and deserialize a response from the server
     fn recv(&mut self) -> Result<Response> {
         let mut reader = BufReader::new(&self.stream);
         let mut buffer = String::new();
@@ -38,23 +44,36 @@ impl Connection {
         Ok(Response::new(buffer))
     }
 
+    /// Sends a message to the in-game chat, does not require a joined player
     pub fn post_to_chat(&mut self, message: impl AsRef<str>) -> Result<()> {
         self.send(Command::new("chat.post").arg_string(message))
     }
 
+    /// Performs an in-game Minecraft command. Players have to exist on the
+    /// server and should be server operators (default with ELCI)
     pub fn do_command(&mut self, command: impl AsRef<str>) -> Result<()> {
         self.send(Command::new("player.doCommand").arg_string(command))
     }
 
+    /// Sets player position (block position of lower half of playermodel) to
+    /// specified [`Coordinate`]
+    ///
+    /// [`Coordinate`]: crate::Coordinate
     pub fn set_player_position(&mut self, position: Coordinate) -> Result<()> {
         self.send(Command::new("player.setPos").arg_coordinate(position))
     }
 
+    /// Sets player position to be one above specified tile (i.e. tile = block
+    /// player is standing on)
     pub fn set_player_tile_position(&mut self, mut position: Coordinate) -> Result<()> {
         position.y += 1;
         self.set_player_position(position)
     }
 
+    /// Returns a [`Coordinate`] representing player position (block position of
+    /// lower half of playermodel)
+    ///
+    /// [`Coordinate`]: crate::Coordinate
     pub fn get_player_position(&mut self) -> Result<Coordinate> {
         self.send(Command::new("player.getPos"))?;
         let response = self.recv()?;
@@ -62,12 +81,18 @@ impl Connection {
         Ok(coord)
     }
 
+    /// Returns the coordinate location of the block the player is standing on
+    /// (i.e. tile)
     pub fn get_player_tile_position(&mut self) -> Result<Coordinate> {
         let mut coord = self.get_player_position()?;
         coord.y -= 1;
         Ok(coord)
     }
 
+    /// Sets block at [`Coordinate`] to specified [`Block`]
+    ///
+    /// [`Block`]: crate::Block
+    /// [`Coordinate`]: crate::Coordinate
     pub fn set_block(&mut self, location: Coordinate, block: Block) -> Result<()> {
         self.send(
             Command::new("world.setBlock")
@@ -76,6 +101,10 @@ impl Connection {
         )
     }
 
+    /// Returns [`Block`] object from specified [`Coordinate`]
+    ///
+    /// [`Block`]: crate::Block
+    /// [`Coordinate`]: crate::Coordinate
     pub fn get_block(&mut self, location: Coordinate) -> Result<Block> {
         self.send(Command::new("world.getBlockWithData").arg_coordinate(location))?;
         let response = self.recv()?;
@@ -83,6 +112,12 @@ impl Connection {
         Ok(block)
     }
 
+    /// Sets a cuboid of blocks to all be the specified [`Block`], with the
+    /// corners of the cuboid specified by [`Coordinate`]s `a` and `b` (in any
+    /// order)
+    ///
+    /// [`Block`]: crate::Block
+    /// [`Coordinate`]: crate::Coordinate
     pub fn set_blocks(&mut self, a: Coordinate, b: Coordinate, block: Block) -> Result<()> {
         self.send(
             Command::new("world.setBlocks")
@@ -92,6 +127,10 @@ impl Connection {
         )
     }
 
+    /// Returns a 3D `Vec` of the [`Block`]s of cuboid specified by
+    ///  [`Coordinate`]s `a` and `b` (in any order)
+    ///
+    /// [`Block`]: crate::Block
     pub fn get_blocks(&mut self, a: Coordinate, b: Coordinate) -> Result<Chunk> {
         self.send(
             Command::new("world.getBlocksWithData")
@@ -104,6 +143,14 @@ impl Connection {
         Ok(chunk)
     }
 
+    /// Returns the `y`-value of the highest solid block at the specified `x`
+    /// and `z` coordinate
+    ///
+    /// **DO NOT USE FOR LARGE AREAS, IT WILL BE VERY SLOW**
+    ///
+    /// Use [`get_heights`] instead
+    ///
+    /// [`get_heights`]: Connection::get_heights
     pub fn get_height(&mut self, x: i32, z: i32) -> Result<i32> {
         self.send(Command::new("world.getHeight").arg_int(x).arg_int(z))?;
         let response = self.recv()?;
@@ -111,6 +158,10 @@ impl Connection {
         Ok(height)
     }
 
+    /// Provides a scaled option of the [`get_height`] call to allow for considerable
+    /// performance gains
+    ///
+    /// [`get_height`]: Connection::get_height
     pub fn get_heights(&mut self, a: Coordinate, b: Coordinate) -> Result<HeightMap> {
         self.send(
             Command::new("world.getHeights")
